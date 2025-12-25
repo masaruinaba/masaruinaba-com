a26b6f79ea26d71dd03ae697dae27c1eb009586a
Masaru Inaba <masaruinaba@me.com>
2025-12-25 23:30:08 +0900

    Update vite.config.ts to define multiple entry points for Rollup build, enhancing project structure and organization.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
-// throw new Error("VITE_CONFIG_LOADED_TEST")
-
-export default defineConfig({});
-
-
-
-
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        v0001: resolve(__dirname, "v0001.html"),
+        v0002: resolve(__dirname, "v0002.html"),
+        v0003: resolve(__dirname, "v0003.html"),
+        v0004: resolve(__dirname, "v0004.html"),
+        v0005: resolve(__dirname, "v0005.html"),
+        v0006: resolve(__dirname, "v0006.html"),
+        sitemap: resolve(__dirname, "sitemap.html"),
+      },
+    },
+  },
+});
