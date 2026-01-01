2d8bec0446f479cfcf7d8977aedf66a5f03913c8
Masaru Inaba <masaruinaba@me.com>
2026-01-01 12:55:28 +0900

    OK!

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
-        v0001: resolve(__dirname, "v0001.html"),
-        v0002: resolve(__dirname, "v0002.html"),
-        v0003: resolve(__dirname, "v0003.html"),
-        v0004: resolve(__dirname, "v0004.html"),
+        v0001: resolve(__dirname, "v0001-mosaic-reveal.html"),
+        v0002: resolve(__dirname, "v0002-eye-following.html"),
+        v0003: resolve(__dirname, "v0003-scroll-text-sequence.html"),
+        v0004: resolve(__dirname, "v0004-letter-morphing.html"),
         sitemap: resolve(__dirname, "sitemap.html"),
         "layered-clock": resolve(__dirname, "apps/layered-clock/index.html"),
         "creative-weather": resolve(__dirname, "apps/creative-weather/index.html"),
